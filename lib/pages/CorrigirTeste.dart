0da0296c56029ed7f8930b4e1291684ed31737a8
Tamara Dimande <tdimande5@gmail.com>
2022-11-26 15:32:49 +0200

    Camera Page added, waiting for debug

--- lib/pages/CorrigirTeste.dart
+++ lib/pages/CorrigirTeste.dart
@@ -2,9 +2,6 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:gallery_saver/gallery_saver.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
 
 class CorrigirTest extends StatefulWidget {
@@ -40,23 +37,6 @@ class _CorrigirTestState extends State<CorrigirTest> {
     } else {
       print("NO any camera found");
     }
-  }
-
-  Future<String?> getDownloadPath() async {
-    Directory? directory;
-    try {
-      if (Platform.isIOS) {
-        directory = await getApplicationDocumentsDirectory();
-      } else {
-        directory = Directory('/storage/emulated/0/Download');
-        // Put file in global download folder, if for an unknown reason it didn't exist, we fallback
-        // ignore: avoid_slow_async_io
-        if (!await directory.exists()) directory = await getExternalStorageDirectory();
-      }
-    } catch (err, stack) {
-      print("Cannot get download folder path");
-    }
-    return directory?.path;
   }
 
   @override
